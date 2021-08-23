9c2f93a430f84723f72a2882b4c95809c57504a0
unknown <kothamanoj288@gmail.com>
2021-08-23 15:28:03 +0530

    code is added

---
+++ src/Com/app/Test.java
@@ -0,0 +1,15 @@
+package Com.app;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		System.out.println("Hi this is manoj");
+		System.out.println(anothermethod());
+	}
+	
+	static int anothermethod(){
+		int n=20;
+		return n;
+	}
+
+}
